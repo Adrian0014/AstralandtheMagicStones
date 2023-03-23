565085b21faa94d9c5a5b57c1c077942429bd826
ElDirolo <rosaldidac@gmail.com>
2023-03-23 13:18:26 +0100

    Mis muertos

---
+++ Assets/PruebaExportacion1/Scripts/KillZone.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    public GameObject player;
+    public Transform respawnPoint;
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Activa");
+        }
+    }
+}
