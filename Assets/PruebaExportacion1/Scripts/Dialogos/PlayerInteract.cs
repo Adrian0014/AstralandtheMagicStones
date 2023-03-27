1b3bffe4e719cc6f6bad883b025f4154df956435
ElDirolo <rosaldidac@gmail.com>
2023-03-27 11:01:47 +0200

    volver hacer AI

--- Assets/PruebaExportacion1/Scripts/Dialogos/PlayerInteract.cs
+++ Assets/PruebaExportacion1/Scripts/Dialogos/PlayerInteract.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerInteract : MonoBehaviour
 {
@@ -21,6 +22,11 @@ public class PlayerInteract : MonoBehaviour
                 {
                     Debug.Log("Entrar a casa1");
                     npcInteractable.EntrarCasa1();
+                }
+                    if(collider.gameObject.CompareTag("PuertaFinal"))
+                {
+                    Debug.Log("Fianl");
+                    SceneManager.LoadScene(2);
                 }
             }
         }
